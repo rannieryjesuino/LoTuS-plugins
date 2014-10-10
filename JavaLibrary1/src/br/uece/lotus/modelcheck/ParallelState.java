1aa262c516da20a4636973698ab2c3b926ae820e
Emerson Lima <emersoncflima@gmail.com>
2014-10-09 21:16:28 -0300

    Reorganized packages and classes Created folder and classes for test of algorithm Separation of algorithm and plugin lotus glue Changed model reachability from toolbar to main menu

--- JavaLibrary1/src/br/uece/lotus/modelcheck/ParallelState.java
+++ JavaLibrary1/src/br/uece/lotus/modelcheck/ParallelState.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.uece.larces.jeri.lotus.parallelComposition;
+package br.uece.lotus.modelcheck;
 
 import br.uece.lotus.State;
 import java.util.Objects;
